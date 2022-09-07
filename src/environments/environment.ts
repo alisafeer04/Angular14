19a2b4e252ea817473c018871ad46fc28e1b08f2
ali abbasi <66187852+alisafeer04@users.noreply.github.com>
2022-09-07 00:09:27 -0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+
+// Your web app's Firebase configuration
+  firebaseConfig : {
+  apiKey: "AIzaSyDk0u95LGolp4KzdrwbqUGRLS9dvDizs1I",
+  authDomain: "fire-crud-641bb.firebaseapp.com",
+  projectId: "fire-crud-641bb",
+  storageBucket: "fire-crud-641bb.appspot.com",
+  messagingSenderId: "350254050779",
+  appId: "1:350254050779:web:3d28d6e6960bc7a5ff8bfe"
+},
   production: false
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
