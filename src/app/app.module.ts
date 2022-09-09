3c6bd6d60064b66d30fac5f236696b08570fa745
ali abbasi <66187852+alisafeer04@users.noreply.github.com>
2022-09-08 18:40:34 -0800

    ali

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,14 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgxSpinnerModule,
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirebaseApp(() => initializeApp( {
+      apiKey: "AIzaSyDk0u95LGolp4KzdrwbqUGRLS9dvDizs1I",
+      authDomain: "fire-crud-641bb.firebaseapp.com",
+      projectId: "fire-crud-641bb",
+      storageBucket: "fire-crud-641bb.appspot.com",
+      messagingSenderId: "350254050779",
+      appId: "1:350254050779:web:3d28d6e6960bc7a5ff8bfe"
+    })),
     provideFirestore(() => getFirestore()),
   ],
   providers: [],
